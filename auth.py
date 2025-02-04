c8c5dee6458656c57d7b902beca3668060868a9d
harshal-ishere <harshal011kamble@gmail.com>
2025-02-04 23:11:25 +0530

    did something

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
 from passlib.context import CryptContext
 from jose import jwt,JWTError
+import os
+from dotenv import load_dotenv
+load_dotenv()
 pwd_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
 
 def hashed_pass(password:str)->str:
@@ -8,17 +11,16 @@ def hashed_pass(password:str)->str:
 def verify_hash_pass(plain_pass:str,hashed_pass:str)->bool:
     return pwd_context.verify(plain_pass,hashed_pass)
 
-SECRET_KEY='fuck_yea_bitch_ass_nigga'
 ALGORITHM='HS256'
 
 def jwt_encode(data:str)->str:
     payload={'sub':str(data)}
-    token=jwt.encode(payload,SECRET_KEY,algorithm=ALGORITHM)
+    token=jwt.encode(payload,os.getenv("SECRET_KEY"),algorithm=ALGORITHM)
     return token
 
 def jwt_decode(token:str):
     try:
-        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-        return payload['sub'] 
+        payload=jwt.decode(token,os.getenv("SECRET_KEY"),algorithms=[ALGORITHM])
+        return payload['sub']
     except JWTError:
         print("error")
