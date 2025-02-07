70f3fb4d061ee0134406b92f3dfc42de1b16d0f5
harshal-ishere <harshal011kamble@gmail.com>
2025-02-07 21:18:10 +0530

    did

--- routes/userRoute.py
+++ routes/userRoute.py
@@ -5,8 +5,8 @@ from schema import Peoples
 from database import Base,engine
 from auth import hashed_pass,verify_hash_pass,jwt_encode,jwt_decode
 router=APIRouter()
-Base.metadata.create_all(bind=engine)
 
+Base.metadata.create_all(bind=engine)
 @router.post("/login",response_model=userResMod)
 async def login(req:userReqMod,db:db_dependancy):
     user=db.query(Peoples)
